1b22cb5cab61064d40c339b511db1f437fed5d86
Lovas Bálint <lovasb93@gmail.com>
2020-11-23 21:19:36 +0100

    Price site finished

--- templates/pages/prices.tpl.php
+++ templates/pages/prices.tpl.php
@@ -1,16 +1,74 @@
 <div class="col-12">
   <h2>Offer</h2>
-  <ul class="mb-4">
-    <li>I offer to feed and carry for your cat when you are out of your apartment in the location of Budapest.</li>
-    <li>I undertake to go your flat two or three times a day as needed. One time supply takes 30 minutes per occasion.</li>
-    <li>If your cat can walk on a leash, I can take him/her for a walk, in other case I provide for a 15-20 minutes play in your home.</li>
-    <li>I take photos or videos what I can send you in email or messenger.</li>
-  </ul>
+  <h5>Cat sitting at your home</h5>
+  <p>I offer to feed and carry for your cat when you are out of your apartment in the location of Budapest. I undertake to go your flat two or three times a day as needed. One time supply takes 30 minutes per occasion. If your cat can walk on a leash, I can take him/her for a walk, in other case I provide for a 15-20 minutes play in your home. I take photos or videos what I can send you in email or messenger.</p>
+  <h5>Cat sitting in my home</h5>
+  <p>If you don’t want me to go your home, you can bring your cat to my place. I have a separate room furnished by cat furniture, full of toys and other funny things. There are three webcameras which you can connect so you can see your feline anytime.</p>
+  <h5>Training and consulting</h5>
+  <p>If you have problems with your cat(s), I can give you some advice to fix them. For example introduce two cats to each other or find solution on behaviour problems.</p>
+
   <h3>Prices</h3>
-  <ul class="mb-4">
-    <li>Service for two times a day: 3 000 HUF</li>
-    <li>Service for three times a day: 4 000 HUF</li>
-    <li>15-30 minutes walk: extra 1 500 HUF</li>
-    <li>Take him/her to the VET for an appointment: extra 1 500 HUF</li>
-  </ul>
+  <h5>Cat sitting</h5>
+  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th colspan="2">At your home</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Service for two times a day</td>
+        <td>3 000 HUF</td>
+      </tr>
+      <tr>
+        <td>Service for three times a day</td>
+        <td>4 000 HUF</td>
+      </tr>
+      <tr>
+        <td>Service for two times a day for five days</td>
+        <td>10 000 HUF</td>
+      </tr>
+      <tr>
+        <td>Service for two three a day for five days</td>
+        <td>13 000 HUF</td>
+      </tr>
+      <tr>
+        <td>15-30 minutes walk</td>
+        <td>Extra 1 500 HUF</td>
+      </tr>
+      <tr>
+        <td>Take him/her to the VET for an appointment</td>
+        <td>Extra 1 500 HUF</td>
+      </tr>
+    </tbody>
+  </table>
+  <table class="table">
+  <thead class="thead-dark">
+      <tr>
+        <th colspan="2">At my home</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Care for two days</td>
+        <td>8 000 HUF</td>
+      </tr>
+      <tr>
+        <td>Care for three days</td>
+        <td>7 000 HUF</td>
+      </tr>
+      <tr>
+        <td>Care for four days</td>
+        <td>6 000 HUF</td>
+      </tr>
+      <tr>
+        <td>Care for five days</td>
+        <td>5 000 HUF</td>
+      </tr>
+      <tr>
+        <td>Extra days</td>
+        <td>2 000 HUF</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
