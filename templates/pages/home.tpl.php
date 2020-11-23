a3d5abf19b137e011dd6d9f235961fe6c59d55cf
Lovas Bálint <lovasb93@gmail.com>
2020-11-23 20:57:42 +0100

    Home page finished

--- templates/pages/home.tpl.php
+++ templates/pages/home.tpl.php
@@ -1,6 +1,28 @@
 <div class="col-12">
   <h2>About me</h2>
   <p>My name is Dorothea Gray and I’m a cat behaviorist. I have worked with cats since 2000 when I adopted my first cat, Sunshine. She had PSTD because her pre-owner abused her for a long time. She bite me for many times when I tried to pet her, so I got to hospital because of my injuries. My friends said that I should take her to the VET to sleep but I decided in a different way. I’ve read all books in this theme and I’ve done a cat behaviour training, shortly after a VET assistant training and finally a breeder training. Now I’m learning for an animal beautician.</p>
+
+  <h3>I have two cats. Let me introduce them.</h3>
+  <div class="row">
+    <div class="col-sm-2">
+      <img class="img-fluid" src="./images/marci.jpg" alt="Picture about Marci">
+    </div>
+    <div class="col-sm-8">
+      <p>Szaffi is a 4-year-old tortoiseshell feline. I got her when she was 8 weeks old. She was a bit shy at first but within a few days, she calmed down. She is a very independent one, she doesn’t need to pet all the time because she doesn’t really like it. She is a bit aggressive, bite and scratch, so need to know and respect her mood. When she was neutering, the assistant said that they need to took her out of the carrier in gloves. So she is a cat of hell. </p>
+      <p>My other male cat’s name is Marzipan. He is a one-and-half years old male cat. He got his name because he is so sweet and kind. Last year on 6th of December a member of the local animal rescuers called me to place a little dirty cat for temporarily. I agreed but asked a cage for him because Szaffi had never seen an other cat before and I didn’t really know how would she reacts. She was furious but the little prince was so nice to her that she leaved him alone but at a safe distance. Marzipan slept in the cage on the first night but the next day he didn’t want to stay in that at all. Finally he slept between us and we fell in love with him so we adopted him. </p>
+      <p>A month passed when they finally didn’t want to kill each other. I started to fed them in the same time and day by day I took their bowl further and further. Eating is a pleasant, positive event and they connect eating and the other one. This is one way to get used to the other.</p>
+    </div>
+    <div class="col-sm-2">
+      <img class="img-fluid" src="./images/szaffi.jpg" alt="Pricture about Szaffi">
+    </div>
+  </div>
   <h5>My role model Jackson Galaxy</h5>
-  <iframe class="mx-auto" width="560" height="315" src="https://www.youtube.com/embed/WzuhuaeS0aQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/WzuhuaeS0aQ" allowfullscreen></iframe>
+  </div>
+  
+  <h5>My special cat training</h5>
+  <video class="img-fluid" controls>
+    <source src="./images/jump.mp4" type="video/mp4" />
+  </video>
 </div>
