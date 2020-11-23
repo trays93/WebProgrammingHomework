2851640a540d90796ca156420387c15e50f509ac
Lovas Bálint <lovasb93@gmail.com>
2020-11-23 21:28:40 +0100

    Contact page finished

--- templates/pages/contact.tpl.php
+++ templates/pages/contact.tpl.php
@@ -4,6 +4,9 @@
   <ul>
     <li><i class="fas fa-phone"></i> 30/1234-8880</li>
     <li><i class="fas fa-envelope"></i> <a href="mailto:thecatsitter@gmail.com">thecatsitter@gmail.com</a></li>
+    <li><i class="fas fa-map"></i> Budapest, Nyírpalota út 14, 1156</li>
   </ul>
-  <iframe class="mx-auto map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2693.406977793443!2d19.13660450095315!3d47.54040734591255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741db1f4a5920a5%3A0x9f6877c5eaee0545!2sBudapest%2C%20Ny%C3%ADrpalota%20%C3%BAt%2014%2C%201156!5e0!3m2!1shu!2shu!4v1605966305599!5m2!1shu!2shu" width="800" height="600" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+  <div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2693.406977793443!2d19.13660450095315!3d47.54040734591255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741db1f4a5920a5%3A0x9f6877c5eaee0545!2sBudapest%2C%20Ny%C3%ADrpalota%20%C3%BAt%2014%2C%201156!5e0!3m2!1shu!2shu!4v1605966305599!5m2!1shu!2shu" allowfullscreen=""></iframe>
+  </div>
 </div>
